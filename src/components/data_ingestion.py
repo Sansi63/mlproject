6de2f1159e2e7220c3e81a24bbe2c34c77430af9
Sansi63 <sinhasanket160@gmail.com>
2026-03-17 21:33:45 +0530

    added model trainer and hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
-
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 
 @dataclass
@@ -51,3 +52,5 @@ if __name__=='__main__':
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
     
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_training(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import os
 import sys
 import pickle
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV 
+from sklearn.metrics import r2_score
 
 def save_model(file_path,obj):
     try:
@@ -11,3 +13,31 @@ def save_model(file_path,obj):
             pickle.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            param=list(params.values())[i]
+            
+            gs=GridSearchCV(model,param,cv=5)
+            gs.fit(X_train,y_train)
+            
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+            
+            train_model_score=r2_score(y_test,y_test_pred)
+            test_model_score=r2_score(y_train,y_train_pred)
+            
+            report[list(models.keys())[i]]=test_model_score
+        return report
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+            
+        
+        
